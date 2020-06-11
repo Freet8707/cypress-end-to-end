e8496df11b17d3e6d258f850bbb802aaee16d5a7
Freet8707 <freet8707@gmail.com>
2020-06-10 21:52:08 -0500

    form test has get and type tests for name and email inputs

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -3,6 +3,11 @@ describe('Form - testing our form inputs', function () {
         cy.visit("http://localhost:3000/")
     })
     it("add text to inputs and submit form", function() {
-        
+        cy.get('[data-cy="name"]')
+        .type("Jeremy")
+        .should("have.value", "Jeremy")
+        cy.get('[data-cy="email"]')
+        .type("freet8707@gmail.com")
+        .should("have.value", "freet8707@gmail.com")
     })
 })
