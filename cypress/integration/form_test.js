d7010af42c32e359b67eb140459425a9a8c1257a
Freet8707 <freet8707@gmail.com>
2020-06-10 21:43:10 -0500

    form-test.js file created

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,8 @@
+describe('Form - testing our form inputs', function () {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/")
+    })
+    it("add text to inputs and submit form", function() {
+        
+    })
+})
